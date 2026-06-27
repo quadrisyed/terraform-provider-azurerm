5c3727a02bb72cd8295fade62246b1027e1c8c7a
quadrisyed <159175231+quadrisyed@users.noreply.github.com>
2026-06-27 11:49:37 -0500

    commit on file changes

--- examples/virtual-machines/linux/basic-password/main.tf
+++ examples/virtual-machines/linux/basic-password/main.tf
@@ -12,7 +12,7 @@ resource "azurerm_resource_group" "main" {
 
 resource "azurerm_virtual_network" "main" {
   name                = "${var.prefix}-network"
-  address_space       = ["10.0.0.0/22"]
+  address_space       = ["10.0.0.0/24"]
   location            = azurerm_resource_group.main.location
   resource_group_name = azurerm_resource_group.main.name
 }
@@ -21,9 +21,18 @@ resource "azurerm_subnet" "internal" {
   name                 = "internal"
   resource_group_name  = azurerm_resource_group.main.name
   virtual_network_name = azurerm_virtual_network.main.name
-  address_prefixes     = ["10.0.2.0/24"]
+  address_prefixes     = ["10.0.0.0/24"]
 }
+resource "azurerm_public_ip" "example" {
+  name                = "acceptanceTestPublicIp1"
+  resource_group_name = azurerm_resource_group.main.name
+  location            = azurerm_resource_group.main.location
+  allocation_method   = "Static"
 
+  tags = {
+    environment = "Production"
+  }
+}
 resource "azurerm_network_interface" "main" {
   name                = "${var.prefix}-nic"
   resource_group_name = azurerm_resource_group.main.name
--- examples/virtual-machines/linux/basic-password/vars.tf
+++ examples/virtual-machines/linux/basic-password/vars.tf
@@ -8,3 +8,11 @@ variable "prefix" {
 variable "location" {
   description = "The Azure Region in which all resources in this example should be created."
 }
+
+variable "username" {
+  description = ""
+}
+
+variable "password" {
+  description = ""
+}
